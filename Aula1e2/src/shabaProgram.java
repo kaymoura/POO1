b0a6a92e384b5f5ed843f6c8efc0104de988d4fa
Kaylane <91162976+kaymoura@users.noreply.github.com>
2024-03-18 22:54:13 -0300

    v1
    Incluindo os codigos

---
+++ Aula1e2/src/shabaProgram.java
@@ -0,0 +1,50 @@
+import java.util.Locale;
+//import java.util.Scanner;
+
+public class shabaProgram {
+    public static void main(String[] args) {
+//        /*System.out.println("Hello word!");
+//         Scanner.sc = new
+//         int float double mod(resto ds divisão)
+//         tarefa instalar eclipse*/
+//        // comando para definir a localização Locale
+//        Locale.setDefault(Locale.US);
+//        int shaba = 32;
+//        double x = 10.35654;
+////        println padrão americano
+//        System.out.println(shaba);
+//        // %n para pula linha.
+//        System.out.printf("%.4f%n", x);
+//        System.out.println("Valor = " + x + " metros");
+//        System.out.printf("Valor = %.2f metros", x );
+//
+//        /* string nome = "shaba"
+//        * int idade = 51
+//        * double renda = 28000*/
+//
+//        String nome = "Shaba";
+//        int idade = 51;
+//        double renda = 28000;
+//
+//        System.out.printf("%s tem %d anos e ganha R$ %.2f reais", nome, idade, renda);
+
+//        int x;
+//        double y;
+//        x = 5; y = 2 * x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+//        int b = 6;
+//        int B = 8;
+//        int h = 5;
+//        int area = ((b + B) / 2) * h;
+//        System.out.println(area);
+
+            int a,b;
+            double resultado;
+            a = 5;
+            b = 2;
+            resultado = (double) a / b;
+        System.out.println(resultado);
+    }
+}
